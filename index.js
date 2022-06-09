9c017deaa25ad8e842a2e43ff658318f7c922b4f
bnagel <nagelb500@gmail.com>
2022-06-08 23:29:06 -0400

    initial inquirer work

--- index.js
+++ index.js
@@ -19,6 +19,71 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`);
-})
+db.connect(() => {
+    promptUser()
+});
+
+const promptUser = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'options',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        }
+    ])
+    .then(userChoice => {
+        switch (userChoice.options) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;       
+        }
+    })
+};
+
+const viewAllEmployees = () => {
+
+};
+
+const addEmployee = () => {
+
+};
+
+const updateEmployeeRole = () => {
+
+};
+
+const viewAllRoles = () => {
+
+};
+
+const addRole = () => {
+
+};
+
+const viewAllDepartments = () => {
+
+};
+
+const addDepartment = () => {
+
+};
