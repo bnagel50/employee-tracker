64de8b050f8fb51bacf3efd54a9ce733cac06b45
bnagel <nagelb500@gmail.com>
2022-06-09 01:38:50 -0400

    fixed connection to db

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require('express');
 const mysql = require('mysql2');
-const console = require('console.table');
+const consoleTable = require('console.table');
 const inquirer = require('inquirer');
+const employeeNames = [];
+const roleNames = [];
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
     {
@@ -15,8 +12,7 @@ const db = mysql.createConnection(
         user: 'root',
         password: 'CARpet!@#456',
         database: 'employee_db'
-    },
-    console.log('Connected to the employee_db database.')
+    }
 );
 
 db.connect(() => {
@@ -61,29 +57,166 @@ const promptUser = () => {
 };
 
 const viewAllEmployees = () => {
-
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.table(results);
+        promptUser();
+    })
 };
 
 const addEmployee = () => {
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter a first name.',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'Enter a last name.',
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: 'Input the ID of the role.',
+            name: 'roleID'
+        },
+        {
+            type: 'input',
+            message: 'Enter the manager ID for the employee.',
+            name: 'manID'
+        }
+    ])
+    .then(res => {
+        db.query('INSERT INTO employee SET ?'),
+        {
+            first_name: res.firstName,
+            last_name: res.lastName,
+            role_id: res.roleID,
+            manager_id: res.manID
+        },
+        (err, res) => {
+            console.log('Employee has been added!');
+            promptUser();
+        }
+    })
 };
 
 const updateEmployeeRole = () => {
-
+    db.query('SELECT CONCAT(employee.first_name, " ", employee.last_name) AS employeeName FROM employees;',
+    function (err, results) {
+        for (let i = 0; i < results.length; i++) {
+            let name = res[i].employeeName;
+            employeeNames.push(name);
+        }
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Which employee is being updated?',
+                name: 'employeeSelection',
+                choices: employeeNames
+            }
+        ])
+        .then((res) => {
+            let newEmp = res.employeeSelection;
+            db.query('SELECT role_tbl.title AS roleTITLE from role_tbl',
+            function(err, results) {
+                for (let i = 0; i < results.length; i++) {
+                let role = results[i].roleTitle;
+                roleNames.push(role);   
+            }
+            inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    message: 'What is the new role for this employee?',
+                    name: 'roleSelection',
+                    choices: roleNames
+                }
+            ])
+            .then(res => {
+                let newRole = res.roleSelection;
+                db.query(`UPDATE employee SET ? WHERE CONCAT(employee.first_name, " ", employee.last_name) = '${newEmp}'; `,
+                {
+                    role_id: roleNames.indexOf(newRole) + 1                
+                },
+                (err, res) => {
+                    console.log('Employee has had their role updated!');
+                    promptUser();
+                });
+            })
+            }
+        )
+        })
+    }
+    )
 };
 
 const viewAllRoles = () => {
-
+    db.query('SELECT * FROM role_tbl', function (err, results) {
+        console.table(results);
+        promptUser();
+    })
 };
 
 const addRole = () => {
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter a role name',
+            name: 'role'
+        },
+        {
+            type: 'input',
+            message: 'Enter a salary for this position (with two decimals).',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'Input the ID of the department.',
+            name: 'departmentID'
+        }
+    ])
+    .then(res => {
+        db.query('INSERT INTO role_tbl SET ?'),
+        {
+            title: res.role,
+            salary: res.salary,
+            department_id: res.departmentID
+        },
+        (err, res) => {
+            console.log('Role has been added!');
+            promptUser();
+        }
+    })
 };
 
 const viewAllDepartments = () => {
-
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+        promptUser();
+    })
 };
 
 const addDepartment = () => {
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter a department name',
+            name: "department"
+        }
+    ])
+    .then(res => {
+        db.query('INSERT INTO department SET ?'),
+        {
+            d_name: res.department
+        },
+        (err, res) => {
+            console.log('Department has been added!');
+            promptUser();
+        }
+    })
 };
